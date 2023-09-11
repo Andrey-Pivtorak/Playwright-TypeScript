a2be6d577ae366a70e8757013dce821c25fa4b71
Andrey <2593147@gmail.com>
2023-09-11 13:39:55 +0200

    create template

---
+++ tests/login.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test';
+import { LoginPage } from '../pages/LoginPage';
+
+test('test', async ({ page }) => {
+
+  const login = new LoginPage(page);
+
+  await login.gotoLoginPage();
+  await login.login('tomsmith', 'SuperSecretPassword!');
+  await page.waitForTimeout(3000); //use only for debugging!
+
+  // await page.goto('https://the-internet.herokuapp.com/login');
+  // await page.getByLabel('Username').click();
+  // await page.getByLabel('Username').fill('tomsmith');
+  // await page.getByLabel('Password').click();
+  // await page.getByLabel('Password').fill('SuperSecretPassword!');
+  // await page.getByRole('button', { name: ' Login' }).click();
+  // await page.goto('https://the-internet.herokuapp.com/secure');
+});
